8dda833c5ea5a5846a9763009e9bae9aeffbeb32
Brandon Kessler <brandonkessler@Brandons-MacBook-Air.local>
2019-09-09 19:50:48 -0700

    added filters and player action aggregator

--- player_stats/core.py
+++ player_stats/core.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 import requests
 import pandas as pd
 
@@ -24,17 +26,17 @@ class AllPlayers:
         Note: Cannot search by both
 
         Args:
-        name 		     First or last name of player
+        name 		 First or last name of player
 
         Returns:
-        players		     A list containing names that match search
+        players		 A list containing names that match search
 
         '''
         players = []
         all_players = self.all_players.copy()
 
         for player in all_players:
-            if name in player.split(' '):
+            if name.lower() in [part.lower() for part in player.split(' ')]:
                 players.append(player)
 
         return players
@@ -101,5 +103,49 @@ class AllPlayers:
         ], ignore_index=True)
 
         self.player_stats = df
+        self._get_player_action()
 
         return print("The 'player_stats' has been saved as a dataframe.")
+
+
+    def drop_preseason(self):
+        mask = self.player_stats['type'] != 'Preseason'
+        self.player_stats = self.player_stats.loc[mask]
+
+        self._get_player_action() # overwrite player action with new stats
+
+        return print('Dropped preseason stats')
+
+
+    def drop_playoffs(self):
+        mask = self.player_stats['type'] != 'Playoffs'
+        self.player_stats = self.player_stats.loc[mask]
+
+        self._get_player_action() # overwrite player action with new stats
+
+        return print('Dropped playoff stats')
+
+
+    def drop_regular_season(self):
+        mask = self.player_stats['type'] != 'Regular'
+        self.player_stats = self.player_stats.loc[mask]
+
+        self._get_player_action() # overwrite player action with new stats
+
+        return print('Dropped regular season stats')
+
+
+    def _get_player_action(self):
+        aggregator = {
+            'minutes': ['count', 'sum']
+        }
+
+        action = self.player_stats.groupby('player').agg(aggregator)
+
+        PlayerAction = namedtuple('PlayerAction', ('total_games', 'total_minutes'))
+        self.player_action = {
+            k: PlayerAction(v[0], v[1]) for k,v in action.iterrows()
+        }
+
+    def averages_per_player(self):
+        pass
